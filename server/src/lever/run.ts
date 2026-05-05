7cb4a78effa7b9ea1fad83bbdf85ac14e60c80d4
vanaigr <vanaigranov@gmail.com>
2026-05-04 20:26:51 -0500

    feat: cap max connections

--- server/src/lever/run.ts
+++ server/src/lever/run.ts
@@ -35,7 +35,7 @@ export async function run(db: BetterSQLite3Database, mainLog: L.Log) {
         mainLog.I('Tiers: ', [tiers.desiredCompanies.length], ', ', [tiers.relevantCompanies.length])
     }, 30 * 60 * 1000)
 
-    const connection = N.createConnection('https://jobs.lever.co')
+    const connection = N.createConnection('https://jobs.lever.co', { connections: 10, pipelining: 5 })
 
     while(true) {
         if(rateLimit) await U.delay(T.Now.instant().add({ seconds: 5 }))
