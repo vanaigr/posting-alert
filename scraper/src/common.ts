a24318c26f8ae00568f60923d627d703dded8472
vanaigr <vanaigranov@gmail.com>
2026-05-14 22:54:08 -0500

    feat: sleep more if no locations to classify

--- scraper/src/common.ts
+++ scraper/src/common.ts
@@ -111,15 +111,17 @@ export async function runPendingNotificationService(db: BetterSQLite3Database, l
 
 export async function runLocationClassificationService(db: BetterSQLite3Database, log: L.Log) {
     while(true) {
-        await U.delay(T.Now.instant().add({ seconds: 1 }))
-
         const row = db.select().from(Db.locationClassification)
             .where(D.eq(Db.locationClassification.isInUs, ''))
             .limit(1)
             .get()
-        if(!row) continue
-
-        await isLocationInUsFull(log, db, row.location)
+        if(row) {
+            await isLocationInUsFull(log, db, row.location)
+            await U.delay(T.Now.instant().add({ seconds: 1 }))
+        }
+        else {
+            await U.delay(T.Now.instant().add({ seconds: 60 }))
+        }
     }
 }
 
