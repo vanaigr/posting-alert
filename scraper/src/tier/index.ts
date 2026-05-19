f929b15b0a8fb0128fed216734c9f762884565a0
vanaigr <vanaigranov@gmail.com>
2026-05-19 13:20:38 -0500

    feat: more filters

--- scraper/src/tier/index.ts
+++ scraper/src/tier/index.ts
@@ -42,7 +42,7 @@ export function isJobRelevant(title: string) {
 }
 
 export function isJobDesired(title: string, description: string | undefined) {
-    const ignoreTitle = /\b(director|lead|manager|staff|supervisor|principal|president|qa|quality|quality assurance|machine learning|head of|servicenow|salesforce|forward deployed)\b/i.test(title)
+    const ignoreTitle = /\b(director|lead|manager|staff|supervisor|principal|president|qa|quality|quality assurance|machine learning|head of|servicenow|salesforce|forward deployed|drupal)\b/i.test(title)
         || /\b(UX)\b/.test(title)
     if(ignoreTitle) return false
 
